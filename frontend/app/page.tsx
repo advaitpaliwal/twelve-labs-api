27e1532e2ef17623451dd8db801d5a9ad397efe4
Advait Paliwal <advaitspaliwal@gmail.com>
2024-01-21 16:37:22 -0500

    docker works

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import { InputFile } from "@/components/InputFile";
-
-import { useRouter } from "next/navigation";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 space-y-4 bg-primary">
-      <div className="flex flex-col items-center justify-center w-3/4 mx-auto">
-        <InputFile />
-      </div>
+      <InputFile />
     </main>
   );
 }
